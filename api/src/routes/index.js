69bfdf5f8d75c70dba0f14e62f135c29de28d83f
Lucas Freijomil <Lucas@MacBook-Pro-de-admin.local>
2023-11-30 23:18:48 -0300

    getGamesByName route and controller working

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const getAllGames = require("../controllers/getAllGames");
 const getVideogameById = require("../controllers/getVideogameById");
-
+const getGamesByName = require("../controllers/getGamesByName");
 
 const router = Router();
 
 router.get("/videogames", getAllGames);
 router.get("/videogames/:idVideogame", getVideogameById);
+router.get("/videogames/name/:name", getGamesByName);
 
 module.exports = router;
