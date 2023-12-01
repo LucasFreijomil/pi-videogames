0a403dbaff9a15310ec658b86f97ff0db850662b
Lucas Freijomil <Lucas@MacBook-Pro-de-admin.local>
2023-11-30 22:17:48 -0300

    getGamesByID controller and route working

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
 const getAllGames = require("../controllers/getAllGames");
+const getVideogameById = require("../controllers/getVideogameById");
+
 
 const router = Router();
 
 router.get("/videogames", getAllGames);
+router.get("/videogames/:idVideogame", getVideogameById);
 
 module.exports = router;
