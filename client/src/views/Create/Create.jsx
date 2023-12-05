1ad8d5c66095e30fd43892a28f97c50503719e1a
Lucas Freijomil <Lucas@MacBook-Pro-de-admin.local>
2023-12-04 23:53:48 -0300

    useCreate custom hook

--- client/src/views/Create/Create.jsx
+++ client/src/views/Create/Create.jsx
@@ -1,111 +1,36 @@
 import Styles from "../Create/Create.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllGenres } from "../../redux/actions";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect } from "react";
+import useCreateHandlers from "../../customHooks/useCreateHandlers";
 
 const Create = () => {
   const dispatch = useDispatch();
+
   const genres = useSelector((state) =>
     state.genres.sort((a, b) => a.name.localeCompare(b.name))
   );
- //IMPORTAR: rating, handleInputChange, form, selectedGenres, handleGenreSelect, handleRemoveGenre, releaseDate, handleReleaseDateChange
- //platformInput, platforms, handleAddPlatform, handleRemovePlatform
+
+  const {
+    rating,
+    handleInputChange,
+    selectedGenres,
+    handleGenreSelect,
+    handleRemoveGenre,
+    releaseDate,
+    handleReleaseDateChange,
+    platformInput,
+    platforms,
+    handleAddPlatform,
+    handleRemovePlatform,
+    handleRatingChange,
+    setPlatformInput,
+    handleFormSubmit,
+  } = useCreateHandlers();
+
   useEffect(() => {
     dispatch(getAllGenres());
   }, []);
-  //RATING HANDLER
-  const [rating, setRating] = useState("");
-  const handleRatingChange = (event) => {
-    setRating(event.target.value);
-  };
- //FORM HANDLER
-  const [form, setForm] = useState({
-    name: "",
-    image: "",
-    description: "",
-  });
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setForm({ ...form, [name]: value });
-  };
-  //GENRE HANDLER
-  const [selectedGenres, setSelectedGenres] = useState({
-    genres: [],
-  });
-  const handleGenreSelect = (event) => {
-    const selectedGenre = event.target.value;
-    if (selectedGenre && !selectedGenres.genres.includes(selectedGenre)) {
-      setSelectedGenres({
-        genres: [...selectedGenres.genres, selectedGenre],
-      });
-    }
-  };
-  const handleRemoveGenre = (genreToRemove) => {
-    const updatedGenres = selectedGenres.genres.filter(
-      (genre) => genre !== genreToRemove
-    );
-    setSelectedGenres({
-      genres: updatedGenres,
-    });
-  };
-  //RELEASED HANDLER
-  const [releaseDate, setReleaseDate] = useState("");
-  const handleReleaseDateChange = (event) => {
-    setReleaseDate(event.target.value);
-  };
-  //PLATFORM HANDLER
-  const [platformInput, setPlatformInput] = useState("");
-  const [platforms, setPlatforms] = useState([]);
-  const handleAddPlatform = () => {
-    if (platformInput.trim() !== "" && !platforms.includes(platformInput)) {
-      setPlatforms([...platforms, platformInput]);
-      setPlatformInput("");
-    }
-  };
-  const handleRemovePlatform = (platformToRemove) => {
-    const updatedPlatforms = platforms.filter(
-      (platform) => platform !== platformToRemove
-    );
-    setPlatforms(updatedPlatforms);
-  };
-
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    console.log("Estado actual del formulario:", form);
-    if (
-      !form.name ||
-      selectedGenres.genres.length === 0 ||
-      platforms.length === 0 ||
-      !form.description ||
-      !form.image ||
-      !releaseDate ||
-      !rating
-    ) {
-      alert("Por favor, completa todos los campos del formulario");
-      return;
-    } else {
-      const completeForm = {
-        name: form.name,
-        image: form.image,
-        description: form.description,
-        platforms: platforms,
-        released: releaseDate,
-        rating: rating,
-        genres: selectedGenres.genres,
-      };
-      try {
-        const { data } = await axios.post(
-          "http://localhost:3001/create",
-          completeForm
-        );
-        alert("New game created!", data);
-        return;
-      } catch (error) {
-        console.error("Error creating new game: ", error);
-      }
-    }
-  };
 
   return (
     <div className={Styles.form}>
